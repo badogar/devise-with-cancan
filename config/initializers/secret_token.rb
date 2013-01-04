b6988e02b7167bf8a9b51c6549f95a8c800dcd84
badogar <badogar@badogar-PC.(none)>
2013-01-04 13:59:13 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bill::Application.config.secret_token = 'c5b184f1ea9aed4723314250c6b7312089f143e9a9ca0a6cce5d4a6f279ad510b125ed59b79f3dc41438b0e569a557b546eea7492106749927ec4395d64d66da'
