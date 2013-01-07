acbe11977464de880a7d8924c7090342243b72ac
Bilal Ahmed <b.ahmeddogar@gmail.com>
2013-01-07 11:33:58 +0500

    ignoring secrets of social networks

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, 'L261t1HhBsyFtHPYPKDRjg', 'IVksuLxHCb5mPyopJdyn9O65uVjDbjKVEOFesR9xts'
+  #provider :facebook, 'APP_ID', 'APP_SECRET'
+  #provider :linked_in, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+end
